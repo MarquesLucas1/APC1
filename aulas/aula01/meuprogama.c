82c3e02b3a486b11624577798ee9e756b7b95e63
MarquesLucas1 <142408397+MarquesLucas1@users.noreply.github.com>
2023-08-23 13:56:10 +0000

    envio para o github

---
+++ aulas/aula01/meuprogama.c
@@ -0,0 +1,17 @@
+/***************************
+
+Autor: Lucas
+DATA: 23/08/2023
+Progama basico em C
+
+****************************/
+#include <stdio.h>
+#include <math.h>
+
+// funcao principal
+
+int main() {
+printf("Teste:\n");
+printf("Raiz quadrada de 16 é %f\n", sqrt(16));
+  return 0;
+}
